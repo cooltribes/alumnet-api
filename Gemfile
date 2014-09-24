f68d90e5a02406d12a8235abed9c28d6548b0508
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-09-24 15:33:19 -0430

    added api structure using versionist generator.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '4.1.6'
 gem 'rails-api'
 gem 'pg'
 gem 'jbuilder'
+gem 'versionist'
 
 group :development do
   gem 'spring'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  api_version(:module => "V1", :header => {:name => "Accept", :value => "application/vnd.alumnet+json; version=1"}) do
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
