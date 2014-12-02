6ab276b2699a287fc1650ade706b05c1b00f81b3
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-12-02 12:06:33 -0430

    Added columns of countries  to profile

--- app/controllers/v1/me/profiles_controller.rb
+++ app/controllers/v1/me/profiles_controller.rb
@@ -25,15 +25,19 @@ class V1::Me::ProfilesController < V1::BaseController
 
     def profile_params
       if @profile.initial?
-        params.permit(:first_name, :last_name, :avatar, :born, :birth_city, :residence_city)
+        params.permit(:first_name, :last_name, :avatar, :born, :birth_city, :residence_city,
+          :bird_country, :residence_country)
       elsif @profile.profile?
         params.permit(contact_infos_attributes: [:contact_type, :info, :privacy])
       elsif @profile.contact? || @profile.experience_a?
-        params.permit(experiences_attributes: [:exp_type, :name, :description, :start_date, :end_date, :city_id, :country_id])
+        params.permit(experiences_attributes: [:exp_type, :name, :description, :start_date,
+          :end_date, :city_id, :country_id])
       elsif @profile.experience_b?
-        params.permit(experiences_attributes: [:exp_type, :name, :description, :start_date, :end_date, :city_id, :country_id, :organization_name])
+        params.permit(experiences_attributes: [:exp_type, :name, :description, :start_date,
+          :end_date, :city_id, :country_id, :organization_name])
       elsif @profile.experience_c?
-        params.permit(experiences_attributes: [:exp_type, :name, :description, :start_date, :end_date, :city_id, :country_id, :organization_name, :internship])
+        params.permit(experiences_attributes: [:exp_type, :name, :description, :start_date,
+          :end_date, :city_id, :country_id, :organization_name, :internship])
       elsif @profile.experience_d?
         params.permit(languages_attributes: [:language_id, :level], skills_attributes: [])
       end
