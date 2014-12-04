267cd0bd7cdab606b8ab0345ba8eac33f5835884
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-12-04 17:50:29 -0430

    minor fix in ProfileController.

--- app/controllers/v1/me/profiles_controller.rb
+++ app/controllers/v1/me/profiles_controller.rb
@@ -31,13 +31,13 @@ class V1::Me::ProfilesController < V1::BaseController
         params.permit(contact_infos_attributes: [:contact_type, :info, :privacy])
       elsif @profile.contact? || @profile.experience_a?
         params.permit(experiences_attributes: [:exp_type, :name, :description, :start_date,
-          :end_date, :city_id, :country_id])
+          :end_date, :city_id, :country_id, :committee_id])
       elsif @profile.experience_b?
         params.permit(experiences_attributes: [:exp_type, :name, :description, :start_date,
-          :end_date, :city_id, :country_id, :organization_name])
+          :end_date, :city_id, :country_id, :organization_name, :committee_id])
       elsif @profile.experience_c?
         params.permit(experiences_attributes: [:exp_type, :name, :description, :start_date,
-          :end_date, :city_id, :country_id, :organization_name, :internship])
+          :end_date, :city_id, :country_id, :organization_name, :internship, :committee_id])
       elsif @profile.experience_d?
         params.permit(languages_attributes: [:language_id, :level], skills_attributes: [])
       end
