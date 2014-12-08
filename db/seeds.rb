82471d4301ed9c644e97741285e2131c1afd46e5
Armando Mendoza <fcoarmandomendoza@gmail.com>
2014-12-08 14:49:24 -0430

    Added csv with committees. Added file with cities.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 require 'smarter_csv'
 
-## Load countries from csv file
 ## THE CITIES MUST BE LOADED IN THE DATABASE DIRECTLY
-## COPY cities (cc_fips, name) FROM 'db/data/GEODATASOURCE-COUNTRY.TXT' DELIMITER E'\t' ENCODING 'ISO-8859-5';
+## COPY cities (cc_fips, name) FROM 'path/to/GEODATASOURCE-CITIES-FREE.TXT' DELIMITER E'\t' ENCODING 'ISO-8859-5';
+
 file = File.open('db/data/GEODATASOURCE-COUNTRY.TXT', "r:ISO-8859-5")
 options = { key_mapping: {country_name: :name}, col_sep: "\t"}
 SmarterCSV.process(file, options ) do |array|
@@ -23,3 +23,10 @@ SmarterCSV.process(file, options ) do |array|
   Skill.create( array.first )
 end
 file.close
+
+file = File.open('db/data/committees.csv', "r:ISO-8859-5")
+SmarterCSV.process(file) do |array|
+  Committee.create( array.first )
+end
+file.close
+
