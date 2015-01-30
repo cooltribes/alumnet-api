68d1cd58600f452875c687c60a1f5fcab81bb258
Nelson Ramirez <nramirez@upsidecorp.ch>
2015-01-30 20:04:57 +0100

    Listos los amigos en comun

--- app/controllers/v1/users/friendships_controller.rb
+++ app/controllers/v1/users/friendships_controller.rb
@@ -1,5 +1,7 @@
 class V1::Users::FriendshipsController < V1::BaseController
   before_action :set_user
+  before_action :set_current_user, only: :commons
+
 
   def index
     @friendships = @user.get_pending_friendships(params[:filter])
@@ -10,10 +12,16 @@ class V1::Users::FriendshipsController < V1::BaseController
   end
 
   def commons
+    friendsA = @current_u.search_accepted_friends(params[:q])    
+    friendsB = @user.search_accepted_friends(params[:q]) 
+    @friends = friendsA & friendsB
   end
 
   private
     def set_user
       @user = User.find(params[:user_id])
+    end
+    def set_current_user
+      @current_u = current_user if current_user      
     end
 end
