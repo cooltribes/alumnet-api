43f8293167864ff98f4e2c18f8aeb627a8cd240e
Armando Mendoza <fcoarmandomendoza@gmail.com>
2015-02-11 16:53:53 -0430

    Added pusher to notify a new notification. Fixes in NotificationController.

--- app/controllers/v1/me/notifications_controller.rb
+++ app/controllers/v1/me/notifications_controller.rb
@@ -1,12 +1,29 @@
 class V1::Me::NotificationsController < V1::BaseController
   before_action :set_user
+  before_action :set_notification, except: [:index, :mark_all_read]
 
   def index
-    @notifications = @user.mailbox.notifications
+    @notifications = @user.mailbox.notifications.limit(params[:limit]).order(created_at: :asc)
+  end
+
+  def mark_as_read
+    @notification.mark_as_read(@user)
+    head :no_content
+  end
+
+  def mark_as_unread
+    @notification.mark_as_unread(@user)
+    head :no_content
+  end
+
+  def mark_all_read
+    @user.mailbox.notifications.each do |notification|
+      notification.mark_as_read(@user)
+    end
+    head :no_content
   end
 
   def destroy
-    @notification = @user.mailbox.notifications.find(params[:id])
     @notification.destroy
     head :no_content
   end
@@ -14,5 +31,9 @@ class V1::Me::NotificationsController < V1::BaseController
   private
     def set_user
       @user = current_user if current_user
+    end
+
+    def set_notification
+      @notification = @user.mailbox.notifications.find(params[:id])
     end
 end
