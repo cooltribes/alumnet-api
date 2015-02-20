aa966c763fde8de4eb0d795853427e3b8fed4cac
Nelson Ramirez <nramirez@upsidecorp.ch>
2015-02-20 16:04:40 -0430

    Showing individual experiences

--- app/views/v1/profiles/experiences/_experience.json.jbuilder
+++ app/views/v1/profiles/experiences/_experience.json.jbuilder
@@ -1,4 +1,4 @@
-json.(experience, :id, :exp_type, :name, :description, :start_date, :end_date, :organization_name,
+json.(experience, :id, :aiesec_experience, :exp_type, :name, :description, :start_date, :end_date, :organization_name,
   :internship, :profile_id)
 
 json.region experience.get_info_region
--- config/routes.rb
+++ config/routes.rb
@@ -67,7 +67,8 @@ Rails.application.routes.draw do
     resources :committees, only: [:index]
 
     resources :profiles, only: [:show, :update] do
-      resources :experiences, except: [:show, :new, :edit], controller: 'profiles/experiences'
+      resources :experiences, except: [:new, :edit], controller: 'profiles/experiences'
+      # resources :experiences, except: [:show, :new, :edit], controller: 'profiles/experiences'
       resources :skills, except: [:show, :new, :edit], controller: 'profiles/skills'
       resources :language_levels, except: [:show, :new, :edit], controller: 'profiles/language_levels'
       resources :contact_infos, except: [:show, :new, :edit], controller: 'profiles/contact_infos'
