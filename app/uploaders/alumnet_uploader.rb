a62aca2819620c4b200d5cdb034740bd9baee104
Armando Mendoza <fcoarmandomendoza@gmail.com>
2015-04-20 17:38:02 -0430

    Added fog gem to handle aws uploads. Refactor in uploads.

---
+++ app/uploaders/alumnet_uploader.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+module AlumnetUploader
+
+  module Base
+
+    def filename
+      if original_filename
+        if model && model.read_attribute(mounted_as).present?
+          model.read_attribute(mounted_as)
+        else
+          "#{secure_token}.#{file.extension}"
+        end
+      end
+    end
+
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+
+    protected
+      def secure_token
+        var = :"@#{mounted_as}_secure_token"
+        model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+      end
+  end
+
+  module Crop
+
+    def crop!
+      if model.imgX1.present?
+        imgW = model.imgW.to_i
+        imgH = model.imgH.to_i
+        imgX1 = model.imgX1.to_i
+        imgY1 = model.imgY1.to_i
+        cropW = model.cropW.to_i
+        cropH = model.cropH.to_i
+        resize_to_limit(imgW, imgH)
+        manipulate! do |img|
+          img.crop "#{cropW}x#{cropH}+#{imgX1}+#{imgY1}"
+        end
+      end
+    end
+  end
+end
