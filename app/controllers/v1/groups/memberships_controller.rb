bcd8ebd7dfab54fd4229438123aeb34c7470a53a
Yondri <yondri@gmail.com>
2015-05-07 17:33:25 -0430

    Mailchimp for official groups, subscribe users

--- app/controllers/v1/groups/memberships_controller.rb
+++ app/controllers/v1/groups/memberships_controller.rb
@@ -1,3 +1,5 @@
+require 'mailchimp'
+
 class V1::Groups::MembershipsController < V1::BaseController
   before_action :set_group
   before_action :set_membership, only: [:update, :destroy]
@@ -18,8 +20,10 @@ class V1::Groups::MembershipsController < V1::BaseController
     @membership = @group.build_membership_for(@user, admin)
     if @membership.save
       @group.notify(@user, admin)
-      @mc_group = Mailchimp::API.new(@group.api_key)
-      @mc_group.lists.subscribe(@group.list_id, {'email' => @user.email}, nil, 'html', false, true, true, true)
+      if @group.mailchimp && @group.join_process == 0
+        @mc_group = Mailchimp::API.new(@group.api_key)
+        @mc_group.lists.subscribe(@group.list_id, {'email' => @user.email}, nil, 'html', false, true, true, true)
+      end
       render :show, status: :created
     else
       render json: @membership.errors, status: :unprocessable_entity
@@ -28,6 +32,14 @@ class V1::Groups::MembershipsController < V1::BaseController
 
   def update
     if @membership.update(membership_params)
+      if @group.mailchimp
+        if @membership.approved
+          @mc_group = Mailchimp::API.new(@group.api_key)
+          @mc_group.lists.subscribe(@group.list_id, {'email' => @membership.user.email}, nil, 'html', false, true, true, true)
+        else
+          @mc_group.lists.unsubscribe(@group.list_id, {'email' => @membership.user.email}, false, false, true)
+        end
+      end
       render :show
     else
       render json: @membership.errors, status: :unprocessable_entity
@@ -35,7 +47,11 @@ class V1::Groups::MembershipsController < V1::BaseController
   end
 
   def destroy
+    email = @membership.user.email
     @membership.destroy
+    if @group.mailchimp
+      @mc_group.lists.unsubscribe(@group.list_id, {'email' => email}, false, false, true)
+    end
     head :no_content
   end
 
--- app/controllers/v1/groups_controller.rb
+++ app/controllers/v1/groups_controller.rb
@@ -30,8 +30,10 @@ class V1::GroupsController < V1::BaseController
     @group.cover_uploader = current_user
     if @group.save
       Membership.create_membership_for_creator(@group, current_user)
-      @mc_group = Mailchimp::API.new(@group.api_key)
-      @mc_group.lists.subscribe(@group.list_id, {'email' => current_user.email}, nil, 'html', false, true, true, true)
+      if @group.mailchimp
+        @mc_group = Mailchimp::API.new(@group.api_key)
+        @mc_group.lists.subscribe(@group.list_id, {'email' => current_user.email}, nil, 'html', false, true, true, true)
+      end
       render :show, status: :created,  location: @group
     else
       render json: @group.errors, status: :unprocessable_entity
