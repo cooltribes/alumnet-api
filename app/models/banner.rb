ef35708b83775e49515e40f8b340cdd5914a8754
Luda <ldcg1@hotmail.com>
2015-06-01 18:37:30 -0430

    Order add in params called in update function of banner controller

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -2,10 +2,7 @@ class Banner < ActiveRecord::Base
   
   mount_uploader :picture, PictureUploader
   
-  def assign_order_to_banner
-  	
-  end			
-  ### Relations
+ ### Relations
   
 
   ### Callbacks
