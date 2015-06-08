2ceedd4a041bfe10a2f67b2cde0d97db21de36d8
Armando Mendoza <fcoarmandomendoza@gmail.com>
2015-06-08 11:40:18 -0430

    Added new model Match.

---
+++ app/models/match.rb
@@ -0,0 +1,4 @@
+class Match < ActiveRecord::Base
+  belongs_to :task
+  belongs_to :user
+end
---
+++ db/migrate/20150608154716_create_matches.rb
@@ -0,0 +1,11 @@
+class CreateMatches < ActiveRecord::Migration
+  def change
+    create_table :matches do |t|
+      t.references :task, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+      t.boolean :applied
+
+      t.timestamps null: false
+    end
+  end
+end
