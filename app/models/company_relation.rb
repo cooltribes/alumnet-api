cda85811574a2ac7a6a81dfbac3c5dc628966f14
Armando Mendoza <fcoarmandomendoza@gmail.com>
2015-06-16 18:12:14 -0430

    Added new methods to update offers and search keywords in CompanyRelation model. Added specs.

--- app/models/company_relation.rb
+++ app/models/company_relation.rb
@@ -12,6 +12,25 @@ class CompanyRelation < ActiveRecord::Base
   validates_presence_of :offer, :search
 
   ### instance methods
+
+  def offer_keywords=(keywords)
+    return false unless keywords.is_a?(Array)
+    offer_keywords.delete_all
+    keywords.each do |kw|
+      keyword = Keyword.find_or_create_by(name: kw)
+      company_relation_keywords.create(keyword: keyword, keyword_type: 0)
+    end
+  end
+
+  def search_keywords=(keywords)
+    return false unless keywords.is_a?(Array)
+    search_keywords.delete_all
+    keywords.each do |kw|
+      keyword = Keyword.find_or_create_by(name: kw)
+      company_relation_keywords.create(keyword: keyword, keyword_type: 1)
+    end
+  end
+
   def offer_keywords
     company_relation_keywords.where(keyword_type: 0)
   end
--- spec/models/company_relation_spec.rb
+++ spec/models/company_relation_spec.rb
@@ -7,5 +7,24 @@ RSpec.describe CompanyRelation, :type => :model do
   it { should validate_presence_of(:offer) }
   it { should validate_presence_of(:search) }
 
+  describe "Instance Methods" do
+    describe "offer_keywords=(keywords)" do
+      it "should set a new keywords and create them if not exists" do
+        company_relation = CompanyRelation.make!
+        5.times { CompanyRelationKeyword.make!(:offer, company_relation: company_relation) }
+        3.times { CompanyRelationKeyword.make!(:search, company_relation: company_relation) }
+        expect(company_relation.offer_keywords.count).to eq(5)
+        new_keyword = Keyword.make!
+        expect {
+          company_relation.offer_keywords =["keywords 1", "keywords 2", new_keyword.name]
+        }.to change(Keyword, :count).by(2)
+        expect(company_relation.offer_keywords.count).to eq(3)
+        expect(company_relation.search_keywords.count).to eq(3)
+      end
+    end
 
+    describe "search_keywords=(keywords)" do
+      it("should be the same as offer_keywords=(keywords)"){}
+    end
+  end
 end
