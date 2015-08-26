ddf552d31a3234fed24da87ac21720a451d26d38
Armando Mendoza <fcoarmandomendoza@gmail.com>
2015-08-25 19:40:56 -0430

    Added CreatePost service to create and post and update user_tags.

---
+++ app/services/posts/create_post.rb
@@ -0,0 +1,24 @@
+module Posts
+  class CreatePost
+
+    attr_reader :postable, :post
+
+    def initialize(postable, current_user, params)
+      @postable = postable
+      @current_user = current_user
+      @params = params
+    end
+
+    def call
+      user_tags_list = @params.delete(:user_tags_list)
+      @post = Post.new(@params)
+      @post.user = @current_user
+      if postable.posts << @post
+        @post.update_user_tags(user_tags_list) if user_tags_list
+        true
+      else
+        false
+      end
+    end
+  end
+end
--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -82,17 +82,8 @@ Experience.blueprint(:profesional) do
   seniority { Seniority.make! }
 end
 
-Group.blueprint do
-  name { "Group #{sn}"}
-  description { Faker::Lorem.sentence }
-  group_type { 0 }
-  join_process { 0 }
-  country { Country.make!(:simple) }
-  city { City.make! }
-  official { false }
-end
 
-Group.blueprint(:with_cover) do
+Group.blueprint do
   name { "Group #{sn}"}
   description { Faker::Lorem.sentence }
   cover { File.open("#{Rails.root}/spec/fixtures/cover_test.jpg") }
