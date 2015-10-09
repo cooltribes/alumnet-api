c3156b638ac9e9faceef44c993f99fa162c1d774
dsepulved <dsepulveda@upsidecorp.ch>
2015-10-09 16:23:50 -0430

    Change copy email request approval

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -40,7 +40,7 @@ class UserMailer < ActionMailer::Base
   def user_request_approval(approver, requester)
     @approver = approver
     @requester = requester
-    mail to: @approver.email, subject: "#{requester.name} wants to be approved in AlumNet"
+    mail to: @approver.email, subject: "#{requester.name}, requested your approval to join AlumNet"
   end
 
   def invitation_to_alumnet(email, guest_name, user, token)
