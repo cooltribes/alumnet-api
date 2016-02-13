0d5ce0911d86000ffb16665f74cba0b6a5f50185
Armando Mendoza <fcoarmandomendoza@gmail.com>
2016-02-12 21:07:59 -0430

    Add relations users to Country model

--- app/models/country.rb
+++ app/models/country.rb
@@ -5,6 +5,7 @@ class Country < ActiveRecord::Base
   has_many :committees, foreign_key: "cc_fips", primary_key: "cc_fips"
   has_many :profiles_residence, class_name: "Profile", foreign_key: "residence_country_id"
   has_many :profiles_birth, class_name: "Profile", foreign_key: "birth_country_id"
+  has_many :users, through: :profiles_residence
   has_many :groups
   has_many :events
   has_many :branches
