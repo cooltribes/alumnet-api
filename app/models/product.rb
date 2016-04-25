ba87af7832421d6de917a788c8ca6ed20bbd0f7e
yondri <yondri@gmail.com>
2016-04-25 11:19:06 -0430

    Product model fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,12 @@
 class Product < ActiveRecord::Base
 	mount_uploader :image, ProductUploader
 	enum status: [:inactive, :active]
+	enum highlight: [:no, :yes]
 
 	# Relations
 	has_many :user_products, dependent: :destroy
 	belongs_to :user
 
 	### Validations
-	validates_presence_of :name, :description, :price
+	validates_presence_of :name, :description
 end
