39fd950a91cbfdbc8d9637d08ff99b59a787ffee
yondrix <yondri@gmail.com>
2016-08-12 17:32:46 -0500

    Fix capistrano bug on deploy (again)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.5.0'
+lock '3.4.0'
 
 set :application, 'alumnet-api'
 set :repo_url, 'https://ArmandoMendoza:14941830famg@github.com/cooltribes/alumnet-api.git '#'git@github.com:cooltribes/alumnet-api.git'
