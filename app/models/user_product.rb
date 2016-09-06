3cf07c5194be715a2a7ff5c189167f7ce565e18a
yondrix <yondri@gmail.com>
2016-09-06 16:02:44 -0400

    Add donors to secret group

--- app/models/user_product.rb
+++ app/models/user_product.rb
@@ -32,8 +32,8 @@ class UserProduct < ActiveRecord::Base
 	      #@active_subscription = UserProduct.where(user_id: @user.id, feature: 'subscription', status: 1).last
 	      active_subscription = UserProduct.where("user_id=? AND status=1 AND (feature='subscription' OR feature='donation')", user.id).last
 	      if active_subscription
-	        self.start_date = active_subscription.end_date
-	        if product_characteristic.present?
+	        self.start_date = active_subscription.end_date.present? ? active_subscription.end_date : DateTime.now
+	        if product_characteristic.present? && active_subscription.end_date.present?
 	          self.end_date = active_subscription.end_date + product_characteristic.value.to_i.months
 	          self.quantity = product_characteristic.value.to_i
 	          user.member = 1
@@ -42,7 +42,7 @@ class UserProduct < ActiveRecord::Base
 	        end
 	      else
 	        self.start_date = DateTime.now
-	        if product_characteristic.present?
+	        if product_characteristic.present? && active_subscription.end_date.present?
 	          self.end_date = DateTime.now + product_characteristic.value.to_i.months
 	          self.quantity = product_characteristic.value.to_i
 	          user.member = 1
@@ -54,9 +54,22 @@ class UserProduct < ActiveRecord::Base
 	    end
 	    if self.save
 	    	user.save
+	    	if feature == 'donation'
+	    		add_user_to_donations_group
+	    	end
 	    	true
 	    else
 	    	false
 	    end
+  	end
+
+  	def add_user_to_donations_group
+  		if Rails.env.production?
+  			group = Group.find(283)
+  			if group.present?
+	  			membership = group.build_membership_for(user, true)
+	    		membership.save
+	    	end
+  		end
   	end
 end
